d9080f5b4e30d9b72d7a2326850b3a019a13ffc6
cherifsirine <126579906+cherifsirine@users.noreply.github.com>
2025-06-08 12:39:13 +0200

    Update script.js

--- script.js
+++ script.js
@@ -440,7 +440,7 @@ if (loginForm) {
     const errorMessage = document.getElementById("loginError");
 
     try {
-      const response = await fetch("http://localhost:3000/api/login", {
+      const response = await fetch(`${API_BASE}/api/login`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ login, password })
